01b8eda59a44d115cfe5c829eed91f4fa3f5a0bb
Facu <Facu@Michelles-MacBook-Pro-2.local>
2022-08-30 18:41:52 +0200

    neumorphism

--- components/Transfer.jsx
+++ components/Transfer.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+
 const TransferUi = () => {
   return (
     <>
@@ -15,6 +16,7 @@ const TransferUi = () => {
         height={40}
         alt='eth'
        />
+  
         <input type='number' id="amount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ETH Amount" required/>
         </div>
         <input type="text" id="sender_address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-8" placeholder="Sender Address" required/>
